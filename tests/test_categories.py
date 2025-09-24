ab5bd3a9001cf08831bbffd648c594062faf37cb
Valentyn <osnikvalentin@gmail.com>
2025-09-24 15:12:34 +0300

    categories test optimization

--- tests/test_categories.py
+++ tests/test_categories.py
@@ -22,17 +22,6 @@ def test_get_categories(user_client):
     assert isinstance(data, list), "Response must be a list"
     assert len(data) > 0, "Categories list should not be empty"
 
-    first_category = data[0]
-    assert "id" in first_category
-    assert isinstance(first_category["id"], int)
-    assert "name" in first_category
-    assert isinstance(first_category["name"], str)
-
-
-def test_get_categories_without_api_key():
-    base_url = os.getenv("BASE_URL")
-    response = requests.get(f"{base_url}categories")  # без headers
-    assert response.status_code in [200, 401], f"Unexpected status: {response.status_code}"
 
 
 def test_categories_schema(user_client):
